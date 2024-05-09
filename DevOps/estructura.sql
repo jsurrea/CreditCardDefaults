0dd4b1396e55e809539ff483cd1d099f6d35df84
HAIDER FONSECA <h.fonseca@uniandes.edu.co>
2024-05-09 11:31:06 -0500

    estructura

---
+++ DevOps/estructura.sql
@@ -0,0 +1,29 @@
+CREATE TABLE customer_data (
+    ID INT,
+    Credit_Limit INT,
+    Sex VARCHAR(10),
+    Education VARCHAR(20),
+    Marriage VARCHAR(20),
+    Age VARCHAR(30),
+    Payed_Status_in_September INT,
+    Payed_Status_in_August INT,
+    Payed_Status_in_July INT,
+    Payed_Status_in_June INT,
+    Payed_Status_in_May INT,
+    Payed_Status_in_April INT,
+    Bill_Amount_in_September INT,
+    Bill_Amount_in_August INT,
+    Bill_Amount_in_July INT,
+    Bill_Amount_in_June INT,
+    Bill_Amount_in_May INT,
+    Bill_Amount_in_April INT,
+    Amount_Paid_in_September INT,
+    Amount_Paid_in_August INT,
+    Amount_Paid_in_July INT,
+    Amount_Paid_in_June INT,
+    Amount_Paid_in_May INT,
+    Amount_Paid_in_April INT,
+    Defaulted_Payment_Next_Month INT
+);
+
+-- Insert data commands should go here, which can be created using the CSV file data.
